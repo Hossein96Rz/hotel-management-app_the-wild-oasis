250026f19be5988c716a07ceab14bc853cc41e1a
Hossein Rezaei <hossein96rz@gmail.com>
2023-11-15 21:11:41 +0330

    some stiles added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
@@ -16,13 +17,15 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="Bookings" element={<Bookings />} />
-          <Route path="Cabins" element={<Cabins />} />
-          <Route path="Users" element={<Users />} />
-          <Route path="Settings" element={<Settings />} />
-          <Route path="Account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="Bookings" element={<Bookings />} />
+            <Route path="Cabins" element={<Cabins />} />
+            <Route path="Users" element={<Users />} />
+            <Route path="Settings" element={<Settings />} />
+            <Route path="Account" element={<Account />} />
+          </Route>
           <Route path="Login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
